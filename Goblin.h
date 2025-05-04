1600f5327a18f253a1648afe7bcde645f04c9ac3
sw1ca <r.swiecicki04@gmail.com>
2025-05-04 13:50:55 +0200

    fix: change positions of goblin

--- Goblin.h
+++ Goblin.h
@@ -16,7 +16,7 @@ private:
     static EnemyConfig createConfig() {
         return {
                 "assets/images/goblin.png",
-                sf::Vector2f(1000.f, 1500.f),
+                sf::Vector2f(300.f, 1500.f),
                 sf::Vector2f(64.f, 64.f),
                 sf::Vector2f(600.f, 600.f),
                 sf::Vector2f(10.f, 10.f),
