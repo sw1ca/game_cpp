0827fb7a0c6ca4ee94c3b022cb8de5d3cfb48d1c
sw1ca <r.swiecicki04@gmail.com>
2025-05-04 13:50:43 +0200

    fix: change positions of skeleton

--- Skeleton.h
+++ Skeleton.h
@@ -10,7 +10,7 @@ private:
     static EnemyConfig CreateConfig() {
         return {
                 "assets/images/spritesheet.png",
-                sf::Vector2f(300.f, 1500.f),
+                sf::Vector2f(275.f, 400.f),
                 sf::Vector2f(64.f, 64.f),
                 sf::Vector2f(600.f, 600.f),
                 sf::Vector2f(10.f, 10.f),
